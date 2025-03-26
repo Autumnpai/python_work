57a6ce4070ab67bf1b7d34998573001383e76ec5
Autumnpai <tangac@gmail.com>
2025-03-25 23:37:48 -0700

    8-11

---
+++ 8-10-sen-msg.py
@@ -0,0 +1,23 @@
+msgs = ['I love you!', 'hug you!', 'kiss you.', 'touch you.']
+def show_msg(mesgs):
+    """print all messages"""
+    for mesg in mesgs:
+        print(mesg)
+
+show_msg(msgs)
+print()
+
+def send_msg(unsent_msgs, sent_msgs):
+    """send message and move the msgs into the sent_msg"""
+    while unsent_msgs:
+        sending_msg = unsent_msgs.pop()
+        print(sending_msg)
+        sent_msgs.append(sending_msg)
+
+sent_mesgs = []
+
+send_msg(msgs, sent_mesgs)
+
+print("\nThe final:")
+print(msgs)
+print(sent_mesgs)
---
+++ 8-9-msg.py
@@ -0,0 +1,7 @@
+msgs = ['I love you!', 'hug you!', 'kiss you.', 'touch you.']
+def show_msg(mesgs):
+    """print all messages"""
+    for mesg in mesgs:
+        print(mesg)
+
+show_msg(msgs)
