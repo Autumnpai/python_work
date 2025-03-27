1cb9cf57c49d7115972ba60eaa785665d8072dae
Autumnpai <tangac@gmail.com>
2025-03-26 23:26:33 -0700

    9-3

---
+++ 9-1-res.py
@@ -0,0 +1,21 @@
+class Restaurant:
+    """make a restaurant class"""
+    def __init__(self, restaurant_name, cuisine_type):
+        """initialize restaurant name and cuisine type"""
+        self.res_name = restaurant_name.title()
+        self.cui_type = cuisine_type
+
+    def describe_restaurant(self):
+        """print the infos"""
+        print(f'The name of the restaurant is {self.res_name}')
+        print(f'The cuisine of the restaurant is {self.cui_type}')
+
+    def open_restanrant(self):
+        """tell the restaurant is open"""
+        print(f'The {self.res_name} restaurant is open!')
+
+
+restaurant = Restaurant('jollybee', 'American')
+print(restaurant.res_name, f'\n{restaurant.cui_type}')
+restaurant.describe_restaurant()
+restaurant.open_restanrant()
---
+++ 9-3-user.py
@@ -0,0 +1,31 @@
+class User:
+    def __init__(self, first_name, last_name, sex, age, education):
+        """initial the class"""
+        self.fir_name = first_name.title()
+        self.las_name = last_name.title()
+        self.x = sex
+        self.edu = education
+        self.ag = age
+    
+    def describe_user(self):
+        print(f'\nThe basic infos of the user:')
+        print(f'Name: {self.fir_name} {self.las_name}')
+        print(f'Sex: {self.x}\nAge: {self.ag}\nEducation: {self.edu}')
+    
+    def greet_user(self):
+        print(f'\nHello {self.fir_name} {self.las_name}!')
+
+
+autumn = User('autumn', 'pai', 'male', 48, 'associate')
+hebe = User('Jiaojiao', 'quan', 'female', 35, 'MBA')
+allan = User('allan', 'tang', 'male', 28, 'bachelor')
+
+autumn.describe_user()
+autumn.greet_user()
+hebe.describe_user()
+hebe.greet_user()
+allan.describe_user()
+allan.greet_user()
+
+
+
