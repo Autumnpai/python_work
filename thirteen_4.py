1676785ed95b5f1286888a7a6b57370ee75c787c
Autumnpai <tangac@gmail.com>
2025-04-10 15:31:24 -0700

    windows

--- thirteen_4.py
+++ thirteen_4.py
@@ -18,21 +18,36 @@ class Raindrops:
     def start_rain(self):
         """Start raining."""
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        sys.exit()
+            self._check_keydown()
             self.screen.fill((135, 206, 235))
             self.drops.draw(self.screen)
             self.drops.update()
             pygame.display.flip()
             self.clock.tick(60)
     
+    def _check_keydown(self):
+        """Check keyboard keypresses"""
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    sys.exit()
+                # Change the speed of the dropping.
+                for drop in self.drops:
+                    if event.key == pygame.K_UP:
+                        drop.speed += 1
+                    if event.key == pygame.K_DOWN and drop.speed > 0:
+                        drop.speed -= 1
+                    if event.key == pygame.K_SPACE:
+                        drop.speed = 0
+    
     def _create_grid(self):
         """ Create the grid of drops. """
+        # Make spacing for vertical 60, and for horizon double/120.
         spacing = 60
-        current_x, current_y = spacing + 30, -1411
-        while current_y < (800 - 12 * spacing):
+        # this variable is for changing starting level. 0 starts from bottom.
+        horizon_ratio = 12
+        current_x, current_y = spacing + 30, -(horizon_ratio * spacing + 91)
+        while current_y < (800 - horizon_ratio * spacing):
             while current_x < (1200 - spacing):
                 drop = Drop(self)
                 drop.rect.x = current_x
@@ -54,13 +69,16 @@ class Drop(pygame.sprite.Sprite):
         # Load the drop image and set its rect attribute.
         self.image = pygame.image.load("drop.png")
         self.rect = self.image.get_rect()
+
+        # Falling speed
+        self.speed = 1
     
     def update(self):
         """ Drops falling down """
         if self.rect.y >= 869:  # check if a drop is READY to go to the top.
             self.rect.y = -91   # change it to the top.
         else:
-            self.rect.y += 1
+            self.rect.y += self.speed
 
 
 rd = Raindrops()
