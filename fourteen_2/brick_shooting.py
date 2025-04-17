a150a039ff6d9ed6e7bb8af06cab4c75a7f5195e
Autumnpai <tangac@gmail.com>
2025-04-16 19:27:52 -0700

    Fourteen 2

--- fourteen_2/brick_shooting.py
+++ fourteen_2/brick_shooting.py
@@ -2,18 +2,101 @@ import sys
 
 import pygame
 
-class Brickshoot:
+from classes import Brick, Bullet, Shooter, Button
+
+class Brickshootgame:
     """A side shooting game to test 'START' button."""
     
     def __init__(self):
-        """Initialize the game."""
+        """Initialize self and assets of the game."""
         pygame.init()
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Brick Shooting")
 
+        self.shooter = Shooter(self)
+        self.brick = Brick(self)
+        self.bullets = pygame.sprite.Group()
+        self.start_button = Button(self, "START")
+
+        self.game_active = False
+        self.missing_limit = 2
+
     def runit(self):
         """Run it!"""
         while True:
-            self.clock.tick(60)
+            self._check_key_press()
+            self.screen.fill((135, 206, 235))
+            if self.game_active:
+                if (self.shooter.moving_down and 
+                    self.shooter.rect.bottom < self.screen.get_rect().bottom):
+                    self.shooter.rect.y += 2
+                if self.shooter.moving_up and self.shooter.rect.top > 0:
+                    self.shooter.rect.y -= 2
+                for bullet in self.bullets.sprites():
+                    bullet.rect.x += 30
+                    if pygame.sprite.spritecollideany(self.brick, self.bullets):
+                        self.bullets.remove(bullet)
+                    if bullet.rect.right >= self.screen.get_rect().right:
+                        self._missing(bullet)
+                    pygame.draw.rect(bullet.screen, bullet.color, bullet.rect)
+            self.shooter.blitme()
+            self.brick.brickmoving()
+            pygame.draw.rect(
+                self.brick.screen, self.brick.color, self.brick.rect)
+            if not self.game_active:
+                self.start_button.draw_button()
+            pygame.display.flip()
 
+            self.clock.tick(60)
+    
+    def _missing(self, bullet):
+        """Respond to the missing shot."""
+        if self.missing_limit > 0:
+            self.bullets.remove(bullet)
+            self.missing_limit -= 1
+        else:
+            self.game_active = False
+    
+    def _fire_bullet(self):
+        """Create and update the bullets"""
+        if len(self.bullets) < 3:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _check_key_press(self):
+        """Check key and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    sys.exit()
+                if event.key == pygame.K_UP:
+                    self.shooter.moving_up = True
+                if event.key == pygame.K_DOWN:
+                    self.shooter.moving_down = True
+                if event.key == pygame.K_SPACE:
+                    self._fire_bullet()
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    self.shooter.moving_up = False
+                if event.key == pygame.K_DOWN:
+                    self.shooter.moving_down = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_start_button(mouse_pos)
+    
+    def _check_start_button(self, mouse_pos):
+        """Start a new game when the player clicks the button."""
+        button_clicked = self.start_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
+            # Reset the game.
+            self.game_active = True
+            self.bullets.empty()
+            self.missing_limit = 2
+            self.shooter.rect.midleft = self.shooter.screen_rect.midleft
+
+
+bs = Brickshootgame()
+bs.runit()
--- fourteen_2/classes.py
+++ fourteen_2/classes.py
@@ -12,15 +12,70 @@ class Shooter:
         self.rect.midleft = self.screen_rect.midleft
         self.moving_up = False
         self.moving_down = False
+    
+    def blitme(self):
+        """Draw the shooter at its current location"""
+        self.screen.blit(self.image, self.rect)
+
 
 class Bullet(pygame.sprite.Sprite):
     """The bullet."""
 
-    def __init__(self, *groups):
-        super().__init__(*groups)
+    def __init__(self, bs):
+        super().__init__()
+        self.screen = bs.screen
+        self.color = (255, 64, 0)
+        self.rect = pygame.Rect(0, 0, 8, 4)
+        self.rect.x = bs.shooter.rect.x + 120
+        self.rect.y = bs.shooter.rect.y + 72
+
 
 class Brick:
     """The brick on the right side."""
 
-    def __init__(self):
-        pass
+    def __init__(self, bs):
+        """Create a ractangle on the right side."""
+        self.screen = bs.screen
+        self.color = (0, 0, 139)  # Dark blue
+        self.rect = pygame.Rect(1100, 0, 100, 200)
+        self.movingdirction = 1
+    
+    def brickmoving(self):
+        """Keep moving the brick up and down."""
+        self.rect.y += 3 * self.movingdirction
+        if self.rect.y <= 0 or self.rect.y >= 600:
+            self.movingdirction *= -1
+
+
+class Button:
+    """A class to build buttons."""
+
+    def __init__(self, bs, msg):
+        """Initialize attributes."""
+        self.screen = bs.screen
+        self.screen_rect = self.screen.get_rect()
+
+        # Set dementions and properties of the button.
+        self.width, self.height = 200, 60
+        self.button_color = (200, 0, 0)
+        self.text_color = (0, 255, 255)
+        self.font = pygame.font.SysFont(None, 64)
+
+        # Build the button's rect object and center it.
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+
+        # The button message needs to be prepped only once.
+        self._prep_msg(msg)
+
+    def _prep_msg(self, msg):
+        """Turn msg into a rendered image and center text on the button."""
+        self.msg_image = self.font.render(msg, True, self.text_color, 
+                                          self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        """Draw blank button and then draw message."""
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
